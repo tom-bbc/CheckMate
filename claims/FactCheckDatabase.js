4ef5f4c6433091403977b3e1a4ea8405ead8f79a
Tom Potter <tom.potter@bbc.co.uk>
2024-09-12 18:42:20 +0100

    save embeddings to fact check database

--- claims/FactCheckDatabase.js
+++ claims/FactCheckDatabase.js
@@ -1,5 +1,5 @@
 const { OpenAI } = require("openai");
-const { dynamoScan, dynamoQuery } = require("../helper/dynamo");
+const { dynamoScan, dynamoQuery, dynamoUpdate } = require("../helper/dynamo");
 const getEmbeddingSimilarity = require("compute-cosine-similarity");
 
 
@@ -18,6 +18,7 @@ const getEmbedding = async (input_text, openai_connection) => {
         model: "text-embedding-3-small",
         input: input_text,
         encoding_format: "float",
+        dimensions: 256
     });
 
     embedding = embedding.data[0].embedding;
@@ -92,14 +93,23 @@ module.exports.getClaimSimilarities = async (input_claim, claim_array, openai_ap
 module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
     // !! UPDATE: check if embedding already in table, use that if so, and if not compute one, use this, and push to table
 
-    console.log("Called fact check database search.")
+    // Set up connection to OpenAI API embedding model
+    let openai;
+    try {
+        openai = new OpenAI({ apiKey: openai_api_key });
+    } catch (error) {
+        console.log(`<!> ERROR: "${error.message}". Cannot set up OpenAI connection. <!>`);
+        return [];
+    }
+
     // Get fact-check database from AWS
     let queryParams = {
         TableName: process.env.FACT_CHECK_TABLE,
-        ProjectionExpression: "#claimID, #claim",
+        ProjectionExpression: "#claimID, #claim, #embedding",
         ExpressionAttributeNames: {
             "#claimID": "claimID",
             "#claim": "claim",
+            "#embedding": "embedding"
         },
         Select: "SPECIFIC_ATTRIBUTES",
     };
@@ -112,29 +122,56 @@ module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
         return [];
     }
 
-    // Extract known claims from database
+    // Get claim embeddings from database or generate via OpenAI
     aws_table_data = aws_table_data.Items;
-    const known_claims = aws_table_data.map(row => row.claim.S);
+    // console.log(aws_table_data);
 
-    // Set up connection to OpenAI API embedding model
-    let openai;
-    try {
-        openai = new OpenAI({ apiKey: openai_api_key });
-    } catch (error) {
-        console.log(`<!> ERROR: "${error.message}". Cannot set up OpenAI connection. <!>`);
-        return [];
-    }
-
-    // Generate embedding for each claim in database and the input claim
+    const regenerate_embeddings = false;
     let claim_embeddings = [];
-    for (const claim of known_claims) {
-        try {
-            let embedding = await getEmbedding(claim, openai);
-            claim_embeddings.push(embedding);
-        } catch (error) {
-            console.log(`<!> ERROR: "${error.message}". Cannot generate embeddings. <!>`);
-            return [];
+
+    for (const row of aws_table_data) {
+        let embedding = [];
+
+        if (!regenerate_embeddings && row.embedding && row.embedding.S !== '') {
+            // Get embedding from database for this claim
+            console.log("Found claim with embedding:", row.claimID.S);
+            embedding = JSON.parse(row.embedding.S);
+        } else {
+            // Generate new embedding
+            console.log("Generating embedding for claim:", row.claimID.S);
+            try {
+                embedding = await getEmbedding(row.claim.S, openai);
+            } catch (error) {
+                console.log(`<!> ERROR: "${error.message}". Cannot generate embeddings. <!>`);
+                return [];
+            }
+
+            // Add new embedding to database
+            queryParams = {
+                "ExpressionAttributeNames": {
+                    "#embedding": "embedding"
+                },
+                "ExpressionAttributeValues": {
+                    ":e": { S: JSON.stringify(embedding) }
+                },
+                "Key": {
+                    "claimID": row.claimID
+                },
+                "ReturnValues": "ALL_NEW",
+                "TableName": process.env.FACT_CHECK_TABLE,
+                "UpdateExpression": "SET #embedding = :e"
+            };
+
+            try {
+                await dynamoUpdate(queryParams);
+            } catch (error) {
+                console.log(`<!> ERROR: "${error.message}". Cannot update fact-check database. <!>`);
+                return [];
+            }
+            console.log("Update item: ", row);
         }
+
+        claim_embeddings.push(embedding);
     }
 
     let input_embedding;
@@ -144,9 +181,11 @@ module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
         console.log(`<!> ERROR: "${error.message}". Cannot generate embeddings. <!>`);
         return [];
     }
+    // console.log(input_embedding);
 
     // Generate similarity scores between input claim and all claims in database
     let similarity_scores = claim_embeddings.map(embedding => getEmbeddingSimilarity(input_embedding, embedding));
+    // console.log(similarity_scores);
 
     // Match the input claim to database claim with maximum similarity score (above threshold)
     const match_threshold = 0.5;
@@ -159,6 +198,7 @@ module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
 
     const max_index = similarity_scores.indexOf(max_similarity);
     const matched_claim_id = aws_table_data[max_index].claimID;
+    // console.log(`Matched claim: ${matched_claim_id} (${max_similarity})`);
 
     // Retrive fact-check data associated with the matched claim from fact-check database
     queryParams = {
@@ -179,6 +219,7 @@ module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
         return [];
     }
     matched_claim_data = matched_claim_data.Items[0];
+    // console.log(matched_claim_data);
 
     // Format fact-check data into output data structure
     const article_url = new URL(matched_claim_data.url.S);
@@ -186,18 +227,18 @@ module.exports.factCheckDatabase = async (input_claim, openai_api_key) => {
 
     const fact_check_results = [{
         factCheckMethod: "Fact check database",
-        matchedClaim: matched_claim_data.claim.S,
-        claimSimilarity: max_similarity,
-        matchedClaimSpeaker: "None",
+        matchedClaim: matched_claim_data.claim?.S,
+        claimSimilarity: Number((100 * max_similarity).toFixed(2)),
+        matchedClaimSpeaker: matched_claim_data.speaker?.S,
         claimReview: [{
             publisher: {
                 name: "None",
                 url: publisher_url_href
             },
             url: article_url.href,
-            title: matched_claim_data.title.S,
-            textualRating: matched_claim_data.textualRating.S,
-            languageCode: matched_claim_data.languageCode.S,
+            title: matched_claim_data.title?.S,
+            textualRating: matched_claim_data.textualRating?.S,
+            languageCode: matched_claim_data.languageCode?.S,
             reviewArticleExtract: "None",
         }]
     }];
--- helper/dynamo.js
+++ helper/dynamo.js
@@ -1,26 +1,55 @@
-const { DynamoDBClient, QueryCommand, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, QueryCommand, ScanCommand, PutItemCommand, UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
 
 
 const client = new DynamoDBClient({});
 
 
+module.exports.dynamoPutItem = async (item, table) => {
+  const input = {
+    Item: item,
+    ReturnConsumedCapacity: "TOTAL",
+    TableName: table,
+  };
+
+  try {
+    const command = new PutItemCommand(input);
+    const response = await client.send(command);
+    return response.item;
+  } catch (error) {
+    console.error("DynamoDB Put Error", error);
+    return false;
+  }
+};
+
+
 module.exports.dynamoQuery = async (params) => {
-    try {
-      const command = new QueryCommand(params);
-      const response = await client.send(command);
-      return response;
-    } catch (error) {
-      console.error("DynamoDB Query Error", error);
-    }
-  };
+  try {
+    const command = new QueryCommand(params);
+    const response = await client.send(command);
+    return response;
+  } catch (error) {
+    console.error("DynamoDB Query Error", error);
+  }
+};
 
 
-  module.exports.dynamoScan = async (params) => {
-    try {
-      const command = new ScanCommand(params);
-      const response = await client.send(command);
-      return response;
-    } catch (error) {
-      console.error("DynamoDB Scan Error", error);
-    }
-  };
+module.exports.dynamoScan = async (params) => {
+  try {
+    const command = new ScanCommand(params);
+    const response = await client.send(command);
+    return response;
+  } catch (error) {
+    console.error("DynamoDB Scan Error", error);
+  }
+};
+
+
+module.exports.dynamoUpdate = async (params) => {
+  try {
+    const command = new UpdateItemCommand(params);
+    const response = await client.send(command);
+    return response;
+  } catch (error) {
+    console.error("DynamoDB Update Error", error);
+  }
+}
